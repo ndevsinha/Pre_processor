89e7021360502cca1ad902cf1740878aea9cb9f8
ndevsinha <ndevsinha@gmail.com>
2020-11-18 18:42:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
   description = 'CSV and json file preprocessing',   # Give a short description about your library
   author = 'Neeraj Kumar',                   # Type in your name
   author_email = 'ndevsinha@gmail.com',      # Type in your E-Mail
-  #url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  url = 'git@github.com:ndevsinha/Pre_processor.git',   # Provide either the link to your github or to your website
+  #download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['CSV PREPROCESSING', 'JSON FLATTENING', 'TEXTUAL DATA CLEANUP'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'random',
