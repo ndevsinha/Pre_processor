4e8f53f34859b3739d878672ec84c8a1b8ce94c6
Neeraj Kumar <neeraj_kumar22@optum.com>
2020-11-24 19:36:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,23 @@
-from distutils.core import setup
-setup(
-  name = 'Pre_processor',         # How you named your package folder (MyLib)
-  packages = ['Pre_processor'],   # Chose the same as "name"
-  version = '0.0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'CSV and json file preprocessing',   # Give a short description about your library
-  author = 'Neeraj Kumar',                   # Type in your name
-  author_email = 'ndevsinha@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/ndevsinha/Pre_processor',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ndevsinha/Pre_processor/archive/v_0.0.2.tar.gz',    # I explain this later on
-  keywords = ['CSV PREPROCESSING', 'JSON FLATTENING', 'TEXTUAL DATA CLEANUP'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'random',
-          'pandas',
-          'json',
-          'nltk',
-          'Stemmer',
-          'string',
-          'itertools',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.6',
-  ],
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Pre_processor", # Replace with your own username
+    version="0.0.5",
+    author="Neeraj Kumar",
+    author_email="neeraj_kumar22@optum.com",
+    description="csv and json file preprocessor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ndevsinha/Pre_processor",
+    download_url = "https://github.com/ndevsinha/Pre_processor/archive/0.04.tar.gz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
